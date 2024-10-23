93a8406ff8c2df391c98555c1a866397090a70cf
Arthur Bazhanau <a.bazhanov@softteco.com>
2024-10-23 17:21:47 +0300

    change host

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,10 @@ import { AppService } from './app.service';
         name: 'PRODUCTS_SERVICE',
         transport: Transport.RMQ,
         options: {
-          urls: ['amqp://guest:guest@rabbitmq:5672'],
+          urls: ['amqp://myuser:mypassword@rabbitmq:5672'],
           queue: 'products_queue',
           queueOptions: {
-            durable: false,
+            durable: true,
           },
         },
       },
--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,10 @@ async function bootstrap() {
     {
       transport: Transport.RMQ,
       options: {
-        urls: ['amqp://guest:guest@rabbitmq:5672'],
+        urls: ['amqp://myuser:mypassword@rabbitmq:5672'],
         queue: 'products_queue',
         queueOptions: {
-          durable: false,
+          durable: true,
         },
       },
     },
