9e9afdfb3f106b122222634b03ae87bf5af4c8f3
Arthur Bazhanau <a.bazhanov@softteco.com>
2024-11-11 18:51:28 +0300

    add grpc

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
+  const app = await NestFactory.create(AppModule);
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: ['amqp://guest:guest@rabbitmq:5672'],
-        queue: 'products_queue',
-        queueOptions: {
-          durable: true,
-        },
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://guest:guest@rabbitmq:5672'],
+      queue: 'products_queue',
+      queueOptions: {
+        durable: true,
       },
     },
-  );
+  });
 
-  await app.listen();
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: 'products',
+      url: '0.0.0.0:3002',
+      protoPath: join(process.cwd(), './infrastructure/protos/products.proto'),
+    },
+  });
+
+  await app.startAllMicroservices();
   logger.log('App has started');
 }
 bootstrap();
